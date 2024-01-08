bdf4d5dfc7d424824b1651dcccd8d73b18ee2d3a
Nikita <mitichkin0303@mail.ru>
2024-01-08 23:14:44 +0200

    Посмотрел урок по пакетам на ютубе

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import Forest.Belka;
+import Forest.Tree;
+public class Test {
+    public static void main(String[] args) {
+        Tree tree1 = new Tree();
+        Belka belka1 = new Belka(); //alt + Enter = autoImport
+
+    }
+}
